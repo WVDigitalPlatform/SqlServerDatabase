86186ec11c9ce7de5b104866e5f44f42c8882dca
WVDigitalPlatform <nick.gilbert@worldvision.ord.uk>
2019-12-13 12:26:45 +0000

    Added a location column

--- Product.sql
+++ Product.sql
@@ -4,5 +4,6 @@
     [First Name] VARCHAR(50) NULL, 
     [Last Name] VARCHAR(50) NULL,
     [Cost] INT NULL, 
-    [Colour] NVARCHAR(50) NULL
+    [Colour] NVARCHAR(50) NULL, 
+    [Location] NVARCHAR(50) NULL
 )
